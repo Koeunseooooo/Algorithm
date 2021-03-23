07635afa5ac09f326830f7bd28d155bd60b99548
koeunseo <rhdmstj17@cau.ac.kr>
2021-03-23 12:37:44 +0900

    두잇-선형탐색실습

---
+++ 두잇알고리즘/검색알고리즘/ssearch_while.py
@@ -0,0 +1,27 @@
+from typing import Sequence, Any
+
+def seq_search(a:Sequence, key: Any) -> int :
+    i=0
+
+    while True:
+        if i==len(a):
+            return -1
+        if a[i]==key:
+            return i
+        i=i+1
+
+if __name__=='__main__':
+    num=int(input('원소 수를 입력하세요.: '))
+    x =[None]*num
+
+    for i in range(num):
+        x[i]=int(input(f'x[{i}]: '))
+
+    ky = int(input('검색할 값을 입력하세요 :'))
+    
+    idx=seq_search(x,ky)
+
+    if idx==-1:
+        print("검색값을 찾는 원소가 존재하지 않습니다")
+    else :
+        print(f'검색값은 x[{idx}]에 위치해 있습니다.')
