9231fe3941eea99704a5f3914acb3c34ef4577fb
koeunseo <rhdmstj17@cau.ac.kr>
2021-04-03 03:50:14 +0900

    solve question2 in assignment2

---
+++ 알고리즘_교과목/assignment2/question2.py
@@ -0,0 +1,14 @@
+#  [Programming] Palindrome refers to words that have the same results when we read from the 
+# beginning and read from the end, such as level, bob, and radar. Write a function that determines if 
+# the given word is palindrome or not. Display the results when you put two different words (one is 
+# palindrome and the other is not).
+
+if __name__=='__main__':
+    word=list(input('Enter a word : '))
+    is_palindrome = True
+    while len(word) > 1 :
+        if word.pop(0) != word.pop():
+            is_palindrome = False
+
+    print("Is word palindrome? Answer: ",is_palindrome)
+    
