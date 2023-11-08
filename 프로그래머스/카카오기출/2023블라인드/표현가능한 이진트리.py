446564de28f9578c9357bc010c8e19a476c0e2a2
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-11-09 01:23:11 +0900

    prog: 카카오-표현가능한이진트리(재풀이)

--- 프로그래머스/카카오기출/2023블라인드/표현가능한 이진트리.py
+++ 프로그래머스/카카오기출/2023블라인드/표현가능한 이진트리.py
@@ -1,42 +1,38 @@
 # https://school.programmers.co.kr/questions/42319
 
 
-# 분할 탐색
-def division_search(left, right, arr):
+import math
+
+
+def division_search(num_bin):
     global flag
-    if left == right:
-        return arr[left]
-    mid = (left + right) // 2
-    if arr[mid] == "0":
-        for i in range(left, mid):
-            if arr[i] == "1":
-                flag = False
-                return
-        for i in range(mid + 1, right + 1):
-            if arr[i] == "1":
-                flag = False
-                return
-    division_search(left, mid - 1, arr)
-    division_search(mid + 1, right, arr)
+    if len(num_bin) == 1:
+        return 1
+    mid = len(num_bin) // 2
+    if num_bin[mid] == "0":
+        for i in range(0, mid):
+            if num_bin[i] == "1":
+                return 0
+        for j in range(mid + 1, len(num_bin)):
+            if num_bin[j] == "1":
+                return 0
+    res1 = division_search(num_bin[:mid])
+    res2 = division_search(num_bin[mid + 1 :])
+    return res1 & res2
 
 
 def solution(numbers):
-    global flag
     answer = []
     for n in numbers:
-        flag = True
-        n = bin(n).replace("0b", "")
-        index = 1
-        node_count = 0
-        while 1:
-            if pow(2, index) - 1 >= len(n):
-                node_count = pow(2, index) - 1
-                break
-            index += 1
-        n = list(("0" * (node_count - len(n))) + n)
-        division_search(0, len(n) - 1, n)
-        if flag:
-            answer.append(1)
+        flag = 1
+        num_bin = bin(n).replace("0b", "")
+        digit = (
+            2 ** (int(math.log(len(num_bin), 2)) + 1) - 1
+        )  # 포화 이진트리를 만들기 위해 필요한 총 노드 개수
+        num_bin = "0" * (digit - len(num_bin)) + num_bin
+        if num_bin[len(num_bin) // 2] == "1":  # 루트노드가 1이라면
+            answer.append(division_search(num_bin))
         else:
             answer.append(0)
+
     return answer
