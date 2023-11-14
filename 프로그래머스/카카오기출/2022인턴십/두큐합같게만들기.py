9501618daafa9361524b5d1f74c089ca458a9dbe
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-11-15 00:57:06 +0900

    prod:두큐합같게만들기(재풀이)

--- 프로그래머스/카카오기출/2022인턴십/두큐합같게만들기.py
+++ 프로그래머스/카카오기출/2022인턴십/두큐합같게만들기.py
@@ -1,1 +1,30 @@
-# 1day 1commit
+from collections import deque
+
+
+def solution(queue1, queue2):
+    answer = 0
+    q1 = deque(queue1)
+    q2 = deque(queue2)
+    sum1 = sum(q1)
+    sum2 = sum(q2)
+    mid = (sum1 + sum2) // 2
+    limit = len(queue1) * 4
+    while True:
+        if sum1 > sum2:
+            tar = q1.popleft()
+            q2.append(tar)
+            sum1 -= tar
+            sum2 += tar
+            answer += 1
+        elif sum1 < sum2:
+            tar = q2.popleft()
+            q1.append(tar)
+            sum1 += tar
+            sum2 -= tar
+            answer += 1
+        else:
+            break
+        if answer == limit:
+            answer = -1
+            break
+    return answer
