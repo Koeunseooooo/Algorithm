15b6bd43f6f3040c2cfdfb22058260242e7b95e4
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-11-18 22:23:57 +0900

    js grammer 정리

---
+++ js대비/소프티어예비test.js
@@ -0,0 +1,70 @@
+// Run by Node.js
+const readline = require("readline");
+
+const d = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+let dy = [-1, 0, 1, 0];
+let dx = [0, 1, 0, -1];
+
+const bfs = (x, y, checked, N, data) => {
+  q = [];
+  q.push([x, y]);
+  checked[x][y] = 1;
+  let cnt = 1;
+  while (q.length) {
+    let [x, y] = q.shift();
+    for (let i = 0; i < 4; i++) {
+      let X = x + dx[i];
+      let Y = y + dy[i];
+      if (
+        0 <= X &&
+        X < N &&
+        0 <= Y &&
+        Y < N &&
+        data[X][Y] === 1 &&
+        checked[X][Y] == 0
+      ) {
+        q.push([X, Y]);
+        checked[X][Y] = 1;
+        cnt += 1;
+      }
+    }
+  }
+  return cnt;
+};
+
+const solution = (N, data) => {
+  let checked = Array.from({ length: N }, () => Array(N).fill(0));
+  let answer = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[0].length; j++) {
+      if ((checked[i][j] === 0) & (data[i][j] === 1)) {
+        answer.push(bfs(i, j, checked, N, data));
+      }
+    }
+  }
+  answer.sort((a, b) => a - b);
+  console.log(answer.length);
+  console.log(answer.join(" "));
+};
+
+(async () => {
+  let rl = readline.createInterface({ input: process.stdin });
+  let N = null;
+  let data = [];
+  let count = 0;
+  for await (const line of rl) {
+    if (!N) {
+      N = +line; // +는 형변환을 바로 해주기 위함임
+    } else {
+      data.push(line.split(" ").map((el) => parseInt(el, 10)));
+      count += 1;
+    }
+    if (count === N) {
+      rl.close();
+    }
+  }
+
+  solution(N, data);
+  process.exit();
+})();
---
+++ js대비/입출력/배열초기화.js
@@ -0,0 +1,18 @@
+var arr = [];
+var arr = [1, 2, 3, 4, 5];
+
+// 1차원 배열
+var arr = Array.from({ length: 5 }, () => 0);
+var arr = Array.from({ length: 5 }, (v, c) => c);
+console.log(arr);
+var arr = new Array(5).fill(0);
+var arr;
+(arr = []).length = 5;
+arr.fill(1);
+console.log(arr);
+
+// 2차원 배열
+var arr = [...Array(5)].map((v, r) =>
+  [...Array(5)].map((v, c) => 5 * r + (c + 1))
+);
+console.log(arr);
