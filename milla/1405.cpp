2a38304703be359e976deb3bc4f9b4730b6b2fd5
Koeunseooooo <rhdmstj17@cau.ac.kr>
2024-08-12 23:35:25 +0900

    progress : 미친 로봇 (20240812)

---
+++ milla/1405.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+// 로봇이 같은 곳을 한 번보다 많이 이동하지 않을 떄 
+// = 로봇이 같은 곳에 한 번만 이동할 때
+
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+
+def dfs(x, y, cnt, p):
+    global ans
+
+    if cnt == n:
+        ans += p
+        return
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if board[nx][ny]:
+            continue
+        if not 0 <= nx < (2 * n) + 1 or not 0 <= ny < (2 * n) + 1:
+            continue
+
+        board[nx][ny] = 1
+        dfs(nx, ny, cnt + 1, p * poss[i] * 0.01)
+        board[nx][ny] = 0
+
+
+n, east, west, south, north = map(int, input().split())
+poss = [north, east, south, west]   # 위의 dx, dy 랑 순서 맞춰줌
+
+board = [[0] * (2 * n + 1) for _ in range(2 * n + 1)]
+board[n][n] = 1
+
+ans = 0
+
+dfs(n, n, 0, 1)
+print(ans)
+
+# python to c++
