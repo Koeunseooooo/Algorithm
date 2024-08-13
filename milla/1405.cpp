fea7ce42308fe171229945e8cd7212fe6ffd87e8
Koeunseooooo <rhdmstj17@cau.ac.kr>
2024-08-14 00:19:55 +0900

    solve: 미친 로봇 (20240814)

--- milla/1405.cpp
+++ milla/1405.cpp
@@ -9,17 +9,31 @@
 using namespace std;
 
 int arr[2*MAX+1][2*MAX+1]={0,}; //0으로 초기화
-int prob[4]; // 동, 서, 남, 북
+double prob[4]; // 동, 서, 남, 북
 int dx[4] = {1,-1,0,0}; // prob와 동일한 순서로
 int dy[4] = {0,0,1,-1};
 int N;
-int result=0;
+double result;
 
 bool isValid(int y, int x){
     return (y>=0 && y<(2*MAX)+1 && x>=0 && x<(2*MAX)+1);
 }
-void dfs(int cur_prob,int cnt, int cur_x, int cur_y){
+
+void print_arr(){
+    cout << "--------------" << endl;
+    for (int i=0;i<(MAX*2)+1;i++){
+        for (int j=0;j<(MAX*2)+1;j++){
+            cout << arr[i][j] << ' ' ;
+        }
+        cout << endl;
+    }
+    cout << "--------------" << endl;
+}
+
+void dfs(double cur_prob,int cnt, int cur_x, int cur_y){
+    // print_arr();
     if(cnt==N){
+        // cout << cur_prob << endl;
         result+=cur_prob;
         return;
     }
@@ -30,12 +44,9 @@ void dfs(int cur_prob,int cnt, int cur_x, int cur_y){
         if (arr[ny][nx]){
             continue;
         }
-        // if (!isValid(ny,nx)){
-        //     continue;
-        // }
+        
         arr[ny][nx]=1;
-        cout << cur_prob*0.01*prob[i] << "test" << endl;
-        dfs(cur_prob*(0.01*prob[i]),cnt+1,nx,ny);
+        dfs(cur_prob*prob[i],cnt+1,nx,ny);
         arr[ny][nx]=0;
     }
 
@@ -43,10 +54,14 @@ void dfs(int cur_prob,int cnt, int cur_x, int cur_y){
 int main(void){
     cin >> N; // N번의 행동을 취할 것(총 이동 N번! 경우의 수는 다양)
     for (int i=0;i<4;i++){
-        cin >> prob[i];
+        int tmp;
+        cin >> tmp;
+        prob[i]=tmp*(0.01);
+        // prob[i]*(0.01);
     }
 
     arr[N][N]=1;
     dfs(1,0,N,N); // cur_prob, cnt(횟수), x,y (배열의 중점에서 시작)
+    cout.precision(10);
     cout << result;
 }
