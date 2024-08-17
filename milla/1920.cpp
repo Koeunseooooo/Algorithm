1d2455bc6d1b67c73c50aff8f6161d8032006ec2
Koeunseooooo <rhdmstj17@cau.ac.kr>
2024-08-17 10:21:14 +0900

    refactor: comment why using "endl" causes errors

--- milla/1920.cpp
+++ milla/1920.cpp
@@ -21,7 +21,8 @@ int main(void){
         cin >> num;
         if(binary_search(v.begin(),v.end(), num)){
             cout << "1" << "\n";
-            // 문제는 endl; 이었다..? endl로 flush 발생 ?
+            // 문제는 endl; endl의 경우 flush() 함수를 겸하기 때문에 실행마다 출력 버퍼를 지워주는 과정(flush)이 생겨
+            // "\n" 보다 속도가 느리다. 따라서 시간 초과가 발생할 경우 endl 대신 "\n"으로 바꿔주면 된다.
         }
         else{
             cout << "0" << "\n";
